60f14b5f8f844eb68e9f8fe97e6db154a9a83193
Harshada <charsh18@gmail.com>
2020-11-19 14:33:56 +0530

    Updates

---
+++ precache-manifest.b0b676d1901d2a8aad309106b86dee08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97d1ae9b8971378fe33aec38d7aa886b",
+    "url": "/ContactList/index.html"
+  },
+  {
+    "revision": "eeeea319b04bb2069100",
+    "url": "/ContactList/static/css/main.1a113df6.chunk.css"
+  },
+  {
+    "revision": "ee36e66937551cd5ba7a",
+    "url": "/ContactList/static/js/2.ccf8f3e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ContactList/static/js/2.ccf8f3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeeea319b04bb2069100",
+    "url": "/ContactList/static/js/main.d7f3b148.chunk.js"
+  },
+  {
+    "revision": "de653de5f1dc42e01dbd",
+    "url": "/ContactList/static/js/runtime-main.77da0399.js"
+  }
+]);
